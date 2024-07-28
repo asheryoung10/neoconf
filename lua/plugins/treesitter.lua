98d038877846ec7d15fc59396972c27f5ddd03f6
= <=>
2024-07-27 22:06:53 -0700

    Fized borked windows

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       local configs = require("nvim-treesitter.configs")
       require 'nvim-treesitter.install'.compilers = { "x86_64-w64-mingw32uwp-clang++" }
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc"},
+          ensure_installed = { "c", },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
